e1642f5bd2a605d8819c05a76343d4210ac179fb
Muhammad Hariz Lazuardi <harizlazuardi01@gmail.com>
2026-07-08 11:20:47 +0700

    Redirect user to dashboard after login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(at: '*');
+        $middleware->redirectUsersTo('/dashboard');
         
         $middleware->alias([
             'admin' => \App\Http\Middleware\IsAdmin::class,
